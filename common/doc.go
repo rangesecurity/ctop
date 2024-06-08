95b420d5e53c450114b4bbe065ad7e5cc5cac98e
bonedaddy <alex@bonedaddy.io>
2024-06-08 16:03:00 -0700

    common: add new_round, new_round_step wrappers, rename field

---
+++ common/doc.go
@@ -0,0 +1,2 @@
+// Package common provides common types and functions reused across multiple packages
+package common
--- common/types.go
+++ common/types.go
@@ -6,10 +6,26 @@ import "time"
 type ParsedVote struct {
 	Type             string
 	Round            int64
-	BlockNumber      int64
+	Height           int64
 	BlockID          string
 	Timestamp        time.Time
 	ValidatorAddress string
 	ValidatorIndex   int64
 	Signature        []byte
+}
+
+// wrapper around types.EventDataNewRound which provides easier to store types
+type ParsedNewRound struct {
+	Height          int64
+	Round           int64
+	Step            string
+	ProposerAddress string
+	ProposerIndex   int64
+}
+
+// wrapper around types.EventDataRoundState which provides easier to store types
+type ParsedNewRoundStep struct {
+	Height int64
+	Round  int64
+	Step   string
 }
